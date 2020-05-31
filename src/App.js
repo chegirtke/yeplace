38035beb6b5ede72803734b71115fa750eea010a
Adilet <chegirtke@gmail.com>
2020-05-31 18:24:17 +0600

    transformed into a react project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <header className="header">
+  <div class="header-contacts">
+  <p> 0555 23 34 45  ikea@gmail.com</p>
+  </div>
+
+ <nav class="header-nav">
+   
+<div class="header-nav_linking">
+  <a href="#" class="header-nav_link">О НАС</a>
+  <a href="#" class="header-nav_link">СВЯЗАТЬСЯ</a>
+ </div>
+</nav>
+<div class="header-button">
+  <button class="button1">Заказать</button>
+ </div>
+</header>
+  )
+}
+
+export default App;
